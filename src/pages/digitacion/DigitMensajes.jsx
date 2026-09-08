18a8f844dd34c1585ea420afcc525c928c451793
BrixnnDev <BrixnnDev@users.noreply.github.com>
2026-09-08 11:54:13 -0500

    Mensajes: eliminar scroll de pagina con margenes negativos y alto de viewport

--- src/pages/digitacion/DigitMensajes.jsx
+++ src/pages/digitacion/DigitMensajes.jsx
@@ -110,7 +110,7 @@ export default function DigitMensajes() {
   const contactoActivo = contactos.find((c) => c.id === activo) || null
 
   return (
-    <div className="flex h-[calc(100vh-7rem)] min-h-0 flex-col overflow-hidden lg:h-[calc(100vh-8rem)]">
+    <div className="-m-6 flex h-[calc(100vh-4rem)] min-h-0 flex-col overflow-hidden lg:-m-8">
       <div className="flex shrink-0 items-center justify-between gap-3 pb-3">
         <div>
           <h2 className="flex items-center gap-2.5 text-xl font-black text-white">
